b2ce25c4b9741e003e24f9eb5b75d261fcc135bf
Closeplanet2 <68482902+Closeplanet2@users.noreply.github.com>
2024-04-13 01:26:52 +0100

    Init Commit

---
+++ src/main/java/com/pandapulsestudios/pulseentity/PulseEntity.java
@@ -0,0 +1,17 @@
+package com.pandapulsestudios.pulseentity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PulseEntity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
